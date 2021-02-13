1aa36e3e607e2031d1ff82814ce9444247f2059a
MayconEduardo <mayconeduardo16@gmail.com>
2021-02-13 00:57:07 -0300

    Adding CreateUserController to the route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { createUserController } from "./useCases/CreateUser";
 
 const router = Router()
 
-router.get('/', (request, response) => {
-    return response.status(200).send()
+router.post('/users', (request, response) => {
+    return createUserController.handle(request, response)
 })
 
 export { router }
